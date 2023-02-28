b75e46820cc951d3760eeadb86151fdccfcff211
ilfa <mhmdilfaj@gmail.com>
2023-02-28 13:01:45 +0700

    Update sw.js

--- sw.js
+++ sw.js
@@ -53,13 +53,12 @@ const CACHE_NAME = 'cool-cache';
 // Add whichever assets you want to precache here:
 const PRECACHE_ASSETS = [
     '/',
-    '/*.html',
     'logo-1-v2.1.jpg',
     '/assets/',
     '/data/',
-    '/news.html',
-    '/teams.html',
-    '/matches.html'
+    'news.html',
+    'teams.html',
+    'matches.html'
 ]
 
 // Listener for the install event - precaches our assets list on service worker install.
